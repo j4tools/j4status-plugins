0e13ff6e0311825674a37078a4039a577ae80e30
Quentin Glidic <sardemff7+git@sardemff7.net>
2014-03-21 17:04:05 +0100

    man: Rework generation
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- src/man.mk
+++ src/man.mk
@@ -1,25 +1,31 @@
 EXTRA_DIST += \
-	src/config.dtd.in \
+	src/config.ent.in \
 	$(man1_MANS:.1=.xml) \
 	$(man5_MANS:.5=.xml) \
 	$(null)
 
 CLEANFILES += \
-	src/config.dtd \
+	src/config.ent \
 	$(man1_MANS) \
 	$(man5_MANS) \
 	$(null)
 
-%.1 %.5: %.xml src/config.dtd
-	$(AM_V_GEN)$(MKDIR_P) $(dir $@) && \
-		$(XSLTPROC) \
-		-o $(dir $@) \
-		$(AM_XSLTPROCFLAGS) \
-		--stringparam profile.condition '$(subst $(space),;,$(XSLTPROC_CONDITIONS))' \
-		http://docbook.sourceforge.net/release/xsl/current/manpages/profile-docbook.xsl \
-		$<
+MAN_GEN_RULE = $(AM_V_GEN)$(MKDIR_P) $(dir $@) && \
+	$(XSLTPROC) \
+	-o $(dir $@) \
+	$(AM_XSLTPROCFLAGS) \
+	--stringparam profile.condition '${AM_DOCBOOK_CONDITIONS}' \
+	$(XSLTPROCFLAGS) \
+	http://docbook.sourceforge.net/release/xsl/current/manpages/profile-docbook.xsl \
+	$<
 
-src/config.dtd: src/config.dtd.in Makefile
+$(man1_MANS): %.1: %.xml $(NKUTILS_MANFILES) src/config.ent
+	$(MAN_GEN_RULE)
+
+$(man5_MANS): %.5: %.xml $(NKUTILS_MANFILES) src/config.ent
+	$(MAN_GEN_RULE)
+
+src/config.ent: src/config.ent.in Makefile
 	$(AM_V_GEN)$(MKDIR_P) $(dir $@) && \
 		$(SED) \
 		-e 's:[@]sysconfdir[@]:$(sysconfdir):g' \
