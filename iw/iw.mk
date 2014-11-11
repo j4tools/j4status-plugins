eb1e445e5b2956f873c25e2e016a6d6deaa80514
Tomsod M <tomsod-m@ya.ru>
2014-11-11 21:35:43 +0300

    iw: Initial draft
    iw plugin shows status of a wireless connection using
    the Linux Wireless Extension API.

---
+++ iw/iw.mk
@@ -0,0 +1,19 @@
+plugins_LTLIBRARIES += \
+	iw/iw.la
+
+iw_iw_la_SOURCES = \
+	iw/src/iw.c
+
+iw_iw_la_CFLAGS = \
+	$(AM_CFLAGS) \
+	$(IW_PLUGIN_CFLAGS) \
+	$(LIBIW_CFLAGS)
+
+iw_iw_la_LDFLAGS = \
+	$(AM_LDFLAGS) \
+	-module -avoid-version -export-symbols-regex j4status_input
+
+iw_iw_la_LIBADD = \
+	$(J4STATUS_PLUGIN_LIBS) \
+	$(IW_PLUGIN_LIBS) \
+	$(LIBIW_LIBS)
