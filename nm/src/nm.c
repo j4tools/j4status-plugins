20bb63d8650da5778ad35dd54e482d51050f43e8
Michal Sojka <michal.sojka@cvut.cz>
2018-08-22 15:13:13 +0200

    nm: Use correct type for SSID
    This fixes the following warning:
    
        warning: assignment to ‘const GByteArray *’ {aka ‘const struct
        _GByteArray *’} from incompatible pointer type ‘GBytes *’ {aka
        ‘struct _GBytes *’} [-Wincompatible-pointer-types]
    
    Signed-off-by: Michal Sojka <michal.sojka@cvut.cz>

--- nm/src/nm.c
+++ nm/src/nm.c
@@ -403,9 +403,9 @@ _j4status_nm_device_update(J4statusPluginContext *context, J4statusNmSection *se
 
                 if ( context->formats.up_wifi_tokens & TOKEN_FLAG_UP_WIFI_SSID )
                 {
-                    const GByteArray *raw_ssid;
-                    raw_ssid = nm_access_point_get_ssid(section->ap);
-                    data.ssid = g_variant_new_take_string(g_strndup((const gchar *)raw_ssid->data, raw_ssid->len));
+                    GBytes *raw_ssid = nm_access_point_get_ssid(section->ap);
+                    data.ssid = g_variant_new_take_string(g_strndup(g_bytes_get_data(raw_ssid, NULL),
+                                                                    g_bytes_get_size(raw_ssid)));
                 }
             }
 
